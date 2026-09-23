8e03b45d508db5db35752b6a921f75501edcb23c
01xumuk01 <01xumuk01@users.noreply.github.com>
2026-09-23 13:26:29 +0000

    Deploying to gh-pages from @ 01xumuk01/OOP@c43410564a61c70daf4341e6c936e8ef41c45151 🚀

--- Task_1_1_2/package-search-index.js
+++ Task_1_1_2/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.oop"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.oop"},{"l":"ru.nsu.oop.console"},{"l":"ru.nsu.oop.game"},{"l":"ru.nsu.oop.model"}];updateSearchResults();
